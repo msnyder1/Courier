c2ece8ce268ad58203cd1e06d5c80e84842ae4fb
Mathew Snyder <snyder.mathew@gmail.com>
2017-10-08 22:17:20 -0400

    Added Angular to the project. Edited homepage.

--- Courier/App_Start/BundleConfig.cs
+++ Courier/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace Courier
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/Angular.min.js"));
         }
     }
 }
